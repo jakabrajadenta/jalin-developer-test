846a28371c2372c24053b3ca5ee5309aa116b9e9
Jaka Brajadenta <79687223+jakabrjdnt@users.noreply.github.com>
2022-08-10 17:48:20 +0700

    Initial project

---
+++ src/main/java/com/jalin/developertest/JalinDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.jalin.developertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalinDeveloperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JalinDeveloperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalin/developertest/JalinDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalin.developertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalinDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
